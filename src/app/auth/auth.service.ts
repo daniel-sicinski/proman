bb9aefa718bc27957a61e78387b846f528ea82f8
Daniel <olgierd.dan@gmail.com>
2019-10-16 19:56:09 +0200

    Add auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,72 @@
+import { Injectable, OnInit } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from "@angular/fire/firestore";
+import { BehaviorSubject } from "rxjs";
+
+interface AuthData {
+  email: string;
+  password: string;
+  name?: string;
+}
+
+interface User {
+  name: string;
+}
+
+@Injectable({
+  providedIn: "root"
+})
+export class AuthService {
+  private usersCollection: AngularFirestoreCollection<User>;
+  private readonly authErrorMessageSubject = new BehaviorSubject<string | null>(
+    null
+  );
+
+  user$ = this.afAuth.user;
+  authErrorMessage$ = this.authErrorMessageSubject.asObservable();
+
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
+    this.usersCollection = this.afs.collection<User>("users");
+  }
+
+  signUp(authData: AuthData): void {
+    const { email, password, name } = authData;
+    this.afAuth.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(userCredential => {
+        const uid = userCredential.user!.uid;
+
+        return this.usersCollection.doc(uid).set({ name });
+      })
+      .catch(err => {
+        this.authErrorMessageSubject.next(err.message);
+      });
+  }
+
+  logIn(authData: AuthData) {
+    const { email, password } = authData;
+    this.afAuth.auth
+      .signInWithEmailAndPassword(email, password)
+      .then(res => {
+        // todo
+        // redirect
+      })
+      .catch(err => {
+        this.authErrorMessageSubject.next(err.message);
+      });
+  }
+
+  logOut() {
+    this.afAuth.auth
+      .signOut()
+      .then(() => {
+        // todo > redirect
+      })
+      .catch(err => {
+        this.authErrorMessageSubject.next(err.message);
+      });
+  }
+}
