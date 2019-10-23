8d5c477f4262a5a1b218a959d693fb996b285e3a
Daniel <olgierd.dan@gmail.com>
2019-10-23 23:00:08 +0200

    Add shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,44 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  MatToolbarModule,
+  MatListModule,
+  MatInputModule,
+  MatButtonModule,
+  MatFormFieldModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatIconModule,
+  MatMenuModule
+} from "@angular/material";
+import { DragDropModule } from "@angular/cdk/drag-drop";
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatListModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatMenuModule,
+    DragDropModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatListModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatMenuModule,
+    DragDropModule
   ]
 })
-export class SharedModule { }
+export class SharedModule {}
