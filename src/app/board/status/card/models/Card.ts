9a0df37e88377afa09a6cd31f96e5f630d8469d6
Daniel <olgierd.dan@gmail.com>
2019-11-22 17:22:13 +0100

    Update Card model

--- src/app/board/status/card/models/Card.ts
+++ src/app/board/status/card/models/Card.ts
@@ -1,5 +1,18 @@
+interface Task {
+  task_name: string;
+  completed: boolean;
+}
+
+interface Checklist {
+  tasks: Task[];
+  title: string;
+}
+
 export interface Card {
+  title: string;
   description: string;
   order: number;
   id?: string;
+  due_date: Date;
+  checklist: Checklist;
 }
