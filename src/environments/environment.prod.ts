a386f6ab6d3a155ec26831a39a6550b078dc136b
Daniel <olgierd.dan@gmail.com>
2019-11-26 12:23:44 +0100

    Add firebase config for prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC6JMRuOI90J-ZkT8swc4JcQePT6WTHPR0",
+    authDomain: "db-demo-a638d.firebaseapp.com",
+    databaseURL: "https://db-demo-a638d.firebaseio.com",
+    projectId: "db-demo-a638d",
+    storageBucket: "db-demo-a638d.appspot.com",
+    messagingSenderId: "893068281588",
+    appId: "1:893068281588:web:efe74115e1dba264d6c0a1",
+    measurementId: "G-Y38XHGTLJ2"
+  }
 };
