e18431052f8130aa00c3cd40ff63fcc5c8ca5bca
Daniel <olgierd.dan@gmail.com>
2019-12-06 09:46:25 +0100

    Add card description display and edit functionality

--- src/app/board/status/card-detail/card-detail.component.ts
+++ src/app/board/status/card-detail/card-detail.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef
+} from "@angular/core";
 import { Card } from "../card/models/Card";
+import { adjustElementHeight } from "src/app/helpers/componentHelpers";
 
 @Component({
   selector: "app-card-detail",
   templateUrl: "./card-detail.component.html",
   styleUrls: ["./card-detail.component.scss"]
 })
-export class CardDetailComponent implements OnInit {
+export class CardDetailComponent {
   @Input() currentCard: Card;
   @Input() statusName: string;
   @Output() closeDisplayCardDetailState: EventEmitter<
     void
   > = new EventEmitter();
 
-  constructor() {}
+  @ViewChild("cardEditContainer", { static: false })
+  cardEditContainer: ElementRef;
 
-  ngOnInit() {}
+  ngAfterViewChecked() {
+    adjustElementHeight(this.cardEditContainer, 25);
+  }
 
   onCloseDisplayCardDetailState(e: Event) {
     if (event) {
